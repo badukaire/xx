fbf8a2b41d2d18ca3cab09a6663da5e7a020a390
Marc GC <carnicer@hp.com>
2017-06-02 12:02:35 +0200

    update as in tecsi and ph

--- rc/bash_aliases.sh
+++ rc/bash_aliases.sh
@@ -1,3 +1,17 @@
+# HP
+alias mmake='make 2>&1 | tee err2.log'
+alias cdx='cd $DJRIP_TARGET_ROOT/bin'
+alias cdw='cd $DJRIP_SOLUTION_ROOT/work'
+alias iTests='time ./internalUnitTests --gtest_output=xml:i.xml ; head -2 i.xml'
+alias eTests='time ./externalAPITests --gtest_output=xml:e.xml ; head -2 e.xml'
+
+export ANALYSIS_TESTS_BASEDIR=/ae/testfiles/test-suites
+
+
+GSOAP_IMPORT=/home/gonmarco/gsoap-2.8/gsoap/import
+
+
+# legacy
 alias ss='svn status'
 alias sup='svnversion ; svn update'
 alias ssq='svn status -q'
@@ -14,9 +28,41 @@ alias sl2='svn log --limit 2'
 alias sl3='svn log --limit 3'
 alias slh='svn log -r HEAD'
 
-alias bdk='bzr diff --using kdiff3'
-alias bl='bzr log | more'
-alias bs='bzr status'
+# git config --global --add alias.lol "log --graph --decorate --pretty=oneline --abbrev-commit --all"
+# git config --global --add alias.low 'log --graph --all --format="%h | %<(21,trunc)%ai | %<(25,trunc)%s"'
+alias g='git status'
+alias gu='git status -uno'
+alias gdi='git diff --internal-diff'
+alias gd='git difftool -t tkdiff'
+alias gdh='git difftool -t tkdiff HEAD'
+alias gdv='git difftool -t vimdiff'
+alias gdg='git difftool -t gvimdiff'
+alias gdt='git diff-tree -r --name-only'
+alias gr='git remote -v'
+alias gl='git lol'
+alias gl9='git lol -9'
+alias gll='git lol -22'
+alias glw='git low'
+alias gmm='git commit -m'
+alias gaA='git add -A'
+alias gau='git add -u'
+alias gpd='git push --delete'
+alias gpa='git push --all'
+alias gpam='git push --all mirror'
+alias gfa='git fetch'
+alias gfam='git fetch mirror'
+alias gur='git update-ref'
+alias gfam='git fetch mirror'
+alias gur='git update-ref'
+alias gri='git rebase -i'
+alias griA='git rebase --abort'
+alias griC='git rebase --continue'
+alias gmff='git merge --ff-only'
+export GRH="refs/heads"
+export GRR="refs/remotes"
+export GRRM=$GRR"/mirror"
+alias gShowB='bash $SCRIPTS/showBranch.sh'
+alias gUpdtB='bash $SCRIPTS/updateBranch.sh'
 
 alias psg='ps ax | grep'
 
@@ -29,9 +75,36 @@ alias la='ls -A'
 alias lah='ls -Ahl'
 alias l='ls -CF --color=auto'
 
+# TODO update for HP
+alias rdt='rdesktop -u remote -d tecsidel -p lediscet -g 1260x940'
+alias rdtt='rdesktop -u remote -p lediscet -g 1260x940'
 
-alias rdtwin='rdesktop -u gonzalezc -p fontvella -d TECSIDEL 172.18.136.60 -g 1240x940'
-alias rdtfj='rdesktop -u remote -p lediscet -d OSLORING 172.24.16.2 -g 1240x940'
-alias rdtalg='rdesktop -u remote -p lediscet 10.4.$SITE.132 -g 1240x940'
-alias rdtDalen='rdesktop -u remote -p lediscet 10.120.5.4 -g 1240x940'
+
+
+
+
+alias l='ls -CF --color=auto'
+alias la='ls -A'
+alias lah='ls -Ahl'
+alias lh='ls -hl --color=auto'
+alias lhr='ls -hlrt --color=auto'
+alias lht='ls -hlrt | tail'
+alias ll='ls -l --color=auto'
+alias ls='ls --color=auto'
+#alias sc='svn commit -m'
+alias sd='svn diff --diff-cmd ~/bin/diff3wrap.sh'
+alias sdv='svn diff --diff-cmd ~/bin/diff3vimdiff.sh'
+alias si='svn info'
+alias sl='svn log'
+alias sl1='svn log --limit 1'
+alias sl2='svn log --limit 2'
+alias sl3='svn log --limit 3'
+alias slh='svn log -r HEAD'
+alias sll='svn log --limit'
+#alias sm='svn merge --diff3-cmd ~/bin/diff3merge.sh'
+alias ss='svn status'
+alias ssq='svn status -q'
+alias ssu='svn status -u'
+alias sup='svnversion ; svn update'
+
 
