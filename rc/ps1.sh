96dbef05ff6d18aa65555673654f4b84d9b45e62
Marc GC <carnicer@hp.com>
2019-04-08 15:21:23 +0200

    add ps1.sh, a sandbox/git prompt script

---
+++ rc/ps1.sh
@@ -0,0 +1,11 @@
+#PS1='[\u@\h \W$(__git_ps1 " ($AE_SANDBOX:%s)")]\$ '
+
+#PS1='[\u@\h \W$(__git_ps1 " \[\e[0;32m\]($AE_SANDBOX:%s)")\[\e[0m\]]\$ '
+
+#PS1='[\u@\h \W]$(__git_ps1 " \[\e[0;32m\]($AE_SANDBOX:%s)")\[\e[0m\]\$ '
+
+#PS1='[\u@\h \W]$(__git_ps1 " \[\e[0;92m\]($AE_SANDBOX\[\e[0m\]:\[\e[0;32m\]%s)")\[\e[0m\]\$ '
+#PS1='[\u@\h \w]$(__git_ps1 " \[\e[0;92m\]($AE_SANDBOX\[\e[0m\]:\[\e[0;32m\]%s)")\[\e[0m\]\$ '
+
+PS1='[\u@\h \[\e[0;94m\]\W\[\e[0m\]]$(__git_ps1 " \[\e[0;32m\]($AE_SANDBOX\[\e[0m\]:\[\e[0;92m\]%s)")\[\e[0m\]\$ '
+
