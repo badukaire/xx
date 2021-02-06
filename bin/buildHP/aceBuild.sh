143e2e367d2de1b449497c094701585b89b7d0b5
Marc GC <carnicer@hp.com>
2021-02-06 11:05:17 +0100

    improved listing, instead of sandbox

--- bin/buildHP/aceBuild.sh
+++ bin/buildHP/aceBuild.sh
@@ -1,24 +1,23 @@
-[ $# -lt 1 ] && { echo "ERROR, missing sandbox" ; exit 1 ; }
-SB=$1
-echo sandbox $SB ...
-[ -d /local/ae/$SB ] || { echo "ERROR, sandbox $SB not found" ; exit 1 ; }
+#!/bin/bash
+
+[ $# -lt 1 ] && { echo "ERROR, missing sandbox / --list option" ; exit 1 ; }
 
 OPT_LIST_BUILD_TYPES="--list"
+
+SB=$1
+[ $SB == $OPT_LIST_BUILD_TYPES ] && echo "don't build, just list build types for all product" || {
+    echo sandbox $SB ...
+    [ -d /local/ae/$SB ] || { echo "ERROR, sandbox $SB not found" ; exit 1 ; }
+}
 
 shift
 [ $# -ge 1 ] && {
     MY_P=$1
-    echo "product: $MY_P"
-    [ $MY_P == $OPT_LIST_BUILD_TYPES ] && {
-        echo "don't build, just list build types for all product"
-        MY_BT=$OPT_LIST_BUILD_TYPES
-    } || {
-        shift
-        [ $# -ge 1 ] && {
-            MY_BT=$1
-            [ $MY_BT == $OPT_LIST_BUILD_TYPES ] && echo "don't build, just list build types for product $MY_P" || echo "build type: $MY_BT"
-            shift
-        }
+    echo "product (abbreviation?): $MY_P"
+    shift
+    [ $# -ge 1 ] && {
+        MY_BT=$1
+        echo "build target (abbreviation?): $MY_BT"
     }
 }
 
@@ -26,8 +25,7 @@ shift
 LOG=aceBuild.log
 ACE=ace
 MM="gmake all"
-#eval [ $MY_BT ] && echo eval MY_BT || echo NO eval MY_BT
-eval [ $MY_BT ] && [ $MY_BT == $OPT_LIST_BUILD_TYPES ] || {
+[ $SB == $OPT_LIST_BUILD_TYPES ] || {
     echo "command: $MM"
     echo "proceed? (ctrl-C to cancel)"
     read OK
@@ -39,15 +37,11 @@ eval [ $MY_BT ] && [ $MY_BT == $OPT_LIST_BUILD_TYPES ] || {
 
 build()
 {
-    eval [ $MY_BT ] && {
-        [ $MY_BT == $OPT_LIST_BUILD_TYPES ] && {
-            #echo "check $P ..."
-            [ $MY_P == $OPT_LIST_BUILD_TYPES -o $MY_P == $P ] && {
-                #echo "MY_P: $MY_P - P: $P"
-                echo "$P $B"
-            }
-            return
-        }
+    [ $SB == $OPT_LIST_BUILD_TYPES ] && {
+        #echo "listing, check $P ..."
+        eval [ $MY_P  ] && { [[ ! $P =~ $MY_P  ]] && return ; }
+        echo "$P $B"
+        return
     }
     #echo "checking for build $P $B ..."
     eval [ $MY_P  ] && { [[ ! $P =~ $MY_P  ]] && return ; }
